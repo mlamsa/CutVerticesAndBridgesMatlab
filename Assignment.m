f3f3a600aa9cc322766aa4c1b7a1d1e3f623c7c7
GoodNoodles <mmkukka@gmail.com>
2019-04-01 21:22:09 +0300

    Actual methods for finding cut vertices and edges. Formulation on DF trees, back edges, bridges and cut vertices in different methods. Execution follows assignment. Algoritm checking is not yet done. Initial debugging and algoritm verification done with the original graph. -Miska

--- Assignment.m
+++ Assignment.m
@@ -1,5 +1,24 @@
 % Groupwork assignment: Algorithmfor finding briges and cut vertices
 % Miska Merikukka & Miia Lämsä
+% Tampere University Discrete mathamatics, 2019 Spring
+%
+% 
+%                              ___....___
+%    ^^                __..-:'':__:..:__:'':-..__
+%                  _.-:__:.-:'':  :  :  :'':-.:__:-._
+%                .':.-:  :  :  :  :  :  :  :  :  :._:'.
+%             _ :.':  :  :  :  :  :  :  :  :  :  :  :'.: _
+%            [ ]:  :  :  :  :  :  :  :  :  :  :  :  :  :[ ]
+%            [ ]:  :  :  :  :  :  :  :  :  :  :  :  :  :[ ]
+%   :::::::::[ ]:__:__:__:__:__:__:__:__:__:__:__:__:__:[ ]:::::::::::
+%   !!!!!!!!![ ]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![ ]!!!!!!!!!!!
+%   ^^^^^^^^^[ ]^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[ ]^^^^^^^^^^^
+%            [ ]                                        [ ]
+%            [ ]                                        [ ]
+%      jgs   [ ]                                        [ ]
+%    ~~^_~^~/   \~^-~^~ _~^-~_^~-^~_^~~-^~_~^~-~_~-^~_^/   \~^ ~~_ ^
+%
+
 
 % Here's some graph functions that might interrest us.
 % At least we can use them to check and visualize our work
@@ -13,10 +32,7 @@ s = [1, 1, 2, 3, 3, 4, 4, 5, 5, 6];         % Start vertices
 t = [2, 6, 6, 4, 7, 5, 7, 7, 8, 7];         % Target vertices
 n = [1:8];
 
-
-G = graph(s, t);                         % Graph object 
-% plot(G);                               % Simple plot
-
+G = graph(s, t);                            % Graph object 
 
 % Also weights can be assigned but this is not relevant in our case. The
 % weights in this list are arbitrary
@@ -29,46 +45,28 @@ G = graph(s, t);                         % Graph object
 % G = graph(s, t, w, n);
 % plot(G,'EdgeLabel',G.Edges.Weight)
 
-% To create a directed graph use Digraph()
+% Create DF forest (T) and back edges (B)
+[T, G, B] = dfForest(G, 7);
 
+% The following executes assignment 1:
+chains = makeChains(G, B, T);
 
-
-% Set of attributes
-
-% for i = 1:8
-% 
-%     
-% end
-
-G.Nodes.visitOrder(8,1) = 0;    % Visit order 1...n - 0 for not visited
-
-
-% Let's go though the original graph G to for the DF tree
-
-% Neighbors of a vertex:
-% ne = neighbors(G, 7) % In initial graph vertex 7 return correctly 3, 5, 6 --> OK!
-
-
-% Build a DF tree
-
-% startNode = 7; % Start node for DF search
-% DFT = dfsearch(G,startNode, 'allevents')
-
-
-
-[T, G, B] = dfForest(G, 7)
+% The following executes assignment 2:
+[bridges, cutV] = bridgesCutV(G, chains)
 
 
 
 function [T, G, B] = dfForest(G, next)
 
-    B = graph();
-    T = digraph();                     % Empty DF tree in the beginning
-    jnum = 1;                   % Start counting orders
-    
-    nodeCount = height(G.Nodes) % Number of nodes inthe graph
+    G.Nodes.visitOrder(8,1) = 0;    % Visit order 1...n - 0 for not visited
 
-    for i = [1:nodeCount]       % 
+    B = digraph();
+    T = digraph();                  % Empty DF tree in the beginning
+    jnum = 1;                       % Start counting orders
+    
+    nodeCount = height(G.Nodes);    % Number of nodes inthe graph
+
+    for i = 1:nodeCount        
         
         if G.Nodes.visitOrder(next) == 0  
             % Visit here
@@ -76,25 +74,32 @@ function [T, G, B] = dfForest(G, next)
         end 
     end
 
+%     Finished product plotted
+    
+%     figure(1)
+%         subplot(1,2,1)
+%             plot(B);
+%             title('Back edges in B');
+%         subplot(1,2,2)
+%             plot(G);
+%             title('Original graph G');
+%         set(gca,'XTick',[], 'YTick', []);
+    
+    
 end
-
-
 
 
 function [B, G, T, jnum, next] = dfRun(jnum, next, G, T, B)
     
     % Get the order number from the for loop in dfForest method - 1 the
     % first time for node next (7)
-    G.Nodes.visitOrder(next) = jnum 
-    
-%     T = [T next]
+    G.Nodes.visitOrder(next) = jnum; 
     
     % Next vertex will get the next order number (one higher)
-    jnum = jnum + 1     
-    
-    
+    jnum = jnum + 1;     
+   
     % Neighboring vertices - where to go next.
-    ne = neighbors(G, next)
+    ne = neighbors(G, next);
     
     % Run through list of neighbors
     for v = 1:length(ne)
@@ -103,20 +108,128 @@ function [B, G, T, jnum, next] = dfRun(jnum, next, G, T, B)
         
         % If the neighbor has not been visited go to
         if G.Nodes.visitOrder(neighborCandi) == 0
-            T = addedge(T, next, neighborCandi)
+
+            T = addedge(T, next, neighborCandi);
             
             % Uncomment to see plotting of DF tree during formation
             % plot(T);
             
-            [B, G, T, jnum, next] = dfRun(jnum, neighborCandi, G, T, B)
-             
+            [B, G, T, jnum, next] = dfRun(jnum, neighborCandi, G, T, B);
+            
             % Setting "current" vertex "back"
-            next = predecessors(T, next)
-                     
+            next = predecessors(T, next);
+            
+        else
+            if findedge(T, neighborCandi, next)
+%                 disp('skip');
+            else
+                
+                % Here the direction detectino mechanism. If the neighbor
+                % has larger visit order number it should be added.
+                
+                if G.Nodes.visitOrder(neighborCandi) > G.Nodes.visitOrder(next) 
+                    B = addedge(B, next, neighborCandi);
+                end
+                % Uncomment to see the graph being built (debugging
+                % purpose)
+%                 plot(B);
+            end
+                  
         end
   
     end
     
+end
+
+
+function chains = makeChains(G, B, T)
+
+    chainNo = 0;
+    
+    chains = {};
+    L = [];
+    
+    
+    % Format visited vertices
+    G.Nodes.visitedForChains(8,1) = 0;
+    
+    % Read table to vector
+    chainCand = B.Edges(:,1);
+    chainCand = chainCand{:,1};
+    
+    for i = 1:length(chainCand)
+        
+        % Mark the initial vertices from back edges as visited
+        G.Nodes.visitedForChains(chainCand(i,1)) = 1;
+        G.Nodes.visitedForChains(chainCand(i,2)) = 1;
+        
+        % Add the vertices to chain
+        L = [L chainCand(i,:)];
+        
+        % Check next one in the hierarchy
+        next = predecessors(T, chainCand(i,2));
+        
+        % While not visited - visit
+        while G.Nodes.visitedForChains(next) == 0
+            L = [L next];
+            next = predecessors(T, next);
+            
+        end
+        % Add the last one
+        L = [L next];
+        
+        % Output to list of chains
+        chains{i} = L;
+        
+        % Format for next round
+        L = [];
+        
+   
+    end
+    
+    
+    
+
+
+
+end
+
+function [bridges, cutV] = bridgesCutV(G, chains)
+    
+     
+    loops = [];
+    bridges = [];
+    cutV = [];
+    
+    % Create loops (edges)
+    for i = 1:length(chains)
+        for j = 1:length(chains{i})-1             
+           loops = [ loops; chains{i}(j) (chains{i}(j+1)) ];          
+        end    
+    end
+    
+    % Read table to vector
+    GEdges = G.Edges(:,1);
+    GEdges = GEdges{:,1};
+    
+%     plot(G) % debug
+    
+    % Remove chain edges from the original graph
+    for i = 1:length(loops)
+        G = rmedge(G, loops(i,1), loops(i,2));
+    end
+    
+    % Format bridges
+    bridges = G.Edges(:,1);
+    bridges = bridges{:,1};
+   
+    % Format cut vertices
+    for i = 1:size(bridges,1)
+        for j = 1:size(bridges,2)
+          cutV = [cutV bridges(i,j)];
+        end
+    end
+
 end
 
 
